4582d4705de44883030b1a8924cdb4040f18bdad
Diego Rivas Lazala <dwr2118@columbia.edu>
2024-09-09 21:01:24 -0400

    Fixed majority of styling issues and initial test still passes

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,28 +1,31 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
-
+  
   @BeforeAll
   public static void setupCourseForTesting() {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
-
-
+  
+  
   @Test
   public void toStringTest() {
-    String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
+    String expectedResult =
+        "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
     assertEquals(expectedResult, testCourse.toString());
   }
-
-  /** The test course instance used for testing. */
+  
+  /**
+   * The test course instance used for testing.
+   */
   public static Course testCourse;
 }
 
